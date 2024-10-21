76a127355cd69aae88b8de212d3464e52283f337
nincy <nincy@Nincy>
2024-10-21 17:26:20 -0600

    Carga Inicial

---
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author nincy
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        
+        Busqueda a= new Busqueda();
+        a.show();
+    }
+}
